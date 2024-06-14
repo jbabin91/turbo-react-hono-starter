e75973a803f68131906d1422a2e0478de0947612
Jace Babin <jbabin91@gmail.com>
2024-06-14 13:48:08 -0700

    refactor(configs): :recycle: reorganize configs package

--- packages/configs/src/index.ts
+++ packages/configs/src/index.ts
@@ -1,5 +1,46 @@
-// Env Variables
-export * from './libs/env';
+import _default, { type Config } from './default';
+import development from './development';
+import production from './production';
 
-// Configs
-export * from './libs/configs';
+export type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
+function isObject(item: object) {
+  return item && typeof item === 'object' && !Array.isArray(item);
+}
+
+function deepMerge<T extends NonNullable<unknown>, U extends Config>(
+  target: T,
+  ...sources: U[]
+): T {
+  if (sources.length === 0) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && source && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key as keyof object])) {
+        if (!target[key as keyof object]) Object.assign(target, { [key]: {} });
+        deepMerge(target[key as keyof object], source[key as keyof object]);
+      } else {
+        Object.assign(target, { [key]: source[key as keyof object] });
+      }
+    }
+  }
+
+  return deepMerge(target, ...sources);
+}
+
+const configModes = {
+  development,
+  production,
+};
+
+export type ConfigMode = keyof typeof configModes;
+
+export const config = deepMerge(
+  _default,
+  configModes[process.env.NODE_ENV as ConfigMode],
+);
