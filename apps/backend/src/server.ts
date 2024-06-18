e4d27ec79848c40fabc7fc934a60e9d844fdd05b
Jace Babin <jbabin91@gmail.com>
2024-06-17 18:35:19 -0700

    chore(backend): test cors wildcard origin
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -29,11 +29,12 @@ app.use(
     allowMethods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'],
     credentials: true,
     exposeHeaders: ['Content-Length', 'Set-Cookie'],
-    origin: (origin) => {
-      console.log('CORS origin', origin);
-      console.log('CORS endsWith', origin.endsWith('.jacebabin.com'));
-      return origin.endsWith('.jacebabin.com') ? origin : config.frontendUrl;
-    },
+    origin: '*',
+    // origin: (origin) => {
+    //   console.log('CORS origin', origin);
+    //   console.log('CORS endsWith', origin.endsWith('.jacebabin.com'));
+    //   return origin.endsWith('.jacebabin.com') ? origin : config.frontendUrl;
+    // },
   }),
 );
 
@@ -41,11 +42,12 @@ app.use(
 app.use(
   '*',
   csrf({
-    origin: (origin) => {
-      console.log('CSRF origin', origin);
-      console.log('CSRF endsWith', origin.endsWith('.jacebabin.com'));
-      return origin.endsWith('.jacebabin.com') || origin === config.frontendUrl;
-    },
+    origin: '*',
+    // origin: (origin) => {
+    //   console.log('CSRF origin', origin);
+    //   console.log('CSRF endsWith', origin.endsWith('.jacebabin.com'));
+    //   return origin.endsWith('.jacebabin.com') || origin === config.frontendUrl;
+    // },
   }),
 );
 
